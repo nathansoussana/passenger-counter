d7f3cb47ca08b6c2ec4108dfff1dcc642fd1bd45
Nathan Soussana <nathan.soussana@gmail.com>
2023-07-23 23:24:00 +0200

    Changed overall style. Added reset button

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ function increment() {
     countEl.textContent = count
 }
 
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
+
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+}
+
+function reset() {
+  saveEl.textContent = "Previous entries: "
 }
